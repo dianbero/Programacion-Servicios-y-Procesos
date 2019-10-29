7b5151530f77b3f94a3cae03c741127624052522
Diana <dianbero@gmail.com>
2019-10-29 12:46:21 +0100

    pff db

--- NBA/DataBase.php
+++ NBA/DataBase.php
@@ -11,6 +11,15 @@ class DataBase
            self::$instance = new self();
        }
        return self::$instance;
+
+       //Carga la configuración config.ini como una array
+       $config = parse_ini_file('config/config.ini');
+
+       //Conectar con la base de datos
+
+       this->connection = new mysqli(); //Terminar
+
+
    }
 
    //Contructor (Probar más tarde)
