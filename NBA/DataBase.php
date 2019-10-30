711a08c8a6e61e1993104c3f2b6e04519faa7589
Diana <dianbero@gmail.com>
2019-10-30 08:33:19 +0100

    asdf

--- NBA/DataBase.php
+++ NBA/DataBase.php
@@ -10,14 +10,14 @@ class DataBase
        if(!(self::$instance instanceof self)){//Si no encuentra instancia de la base de datos, crea una nueva
            self::$instance = new self();
        }
-       return self::$instance;
+       return self::$instance; //Devuelve la nueva base de datos creada
 
        //Carga la configuración config.ini como una array
        $config = parse_ini_file('config/config.ini');
 
        //Conectar con la base de datos
 
-       this->connection = new mysqli(); //Terminar
+       this->connection = new mysqli($config['']); //Terminar
 
 
    }
