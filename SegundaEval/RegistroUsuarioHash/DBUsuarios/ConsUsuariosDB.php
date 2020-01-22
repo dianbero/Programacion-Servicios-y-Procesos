934078e1168afe787e1bf0d3f7194c52db77ce83
Diana <dianbero@gmail.com>
2020-01-22 10:23:31 +0100

    sale el formulario en index.html, yata

---
+++ SegundaEval/RegistroUsuarioHash/DBUsuarios/ConsUsuariosDB.php
@@ -0,0 +1,11 @@
+<?php
+namespace ConstantesUsuarios;
+
+class ConsUsuariosDB
+{
+    const TABLE_NAME = "Usuarios";
+    const COD = "idUsuario";
+    const NOMBRE = "nombreUsuario";
+    const PASSWORD = "password";
+
+}
---
+++ SegundaEval/RegistroUsuarioHash/Usuario.php
@@ -0,0 +1,66 @@
+<?php
+
+
+class Usuario
+{
+    private $idUsuario;
+    private $nombreUsuario;
+    private $password;
+
+    public function _construct($idUsuario, $nombreUsuario, $password){
+        $this->idUsuario = $idUsuario;
+        $this->nombreUsuario = nombreUsuario;
+        $this->password = $password;
+}
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * @param mixed $idUsuario
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombreUsuario()
+    {
+        return $this->nombreUsuario;
+    }
+
+    /**
+     * @param mixed $nombreUsuario
+     */
+    public function setNombreUsuario($nombreUsuario)
+    {
+        $this->nombreUsuario = $nombreUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+}
