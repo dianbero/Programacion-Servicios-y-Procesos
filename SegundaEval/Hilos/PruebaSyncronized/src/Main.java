55374f1c60c16abe13145102ad585c49f81ffe2a
Diana <dianbero@gmail.com>
2020-03-03 09:26:21 +0100

    cosas

--- SegundaEval/Hilos/PruebaSyncronized/src/Main.java
+++ SegundaEval/Hilos/PruebaSyncronized/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //Aquí van las llamadas a start() etc
+        Recursos recursos = new Recursos();
+        Runnable productor = new Productor(recursos);
+        Thread hiloProductor = new Thread(productor);
+
+        Thread hiloConsumidor = new Thread();
     }
 }
